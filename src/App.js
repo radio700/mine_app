a4ac8ba22a54f17d52b3ee6d1bed6c29022914b0
jawsbar <radio700@hanmail.net>
2023-07-27 23:10:55 +0900

    login_start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
-import {Routes, Route, BrowserRouter, Link} from "react-router-dom"
-import Home from "./routes/Home";
-import Detail from "./routes/Detail";
-import Example from "./Example";
+import { useEffect, useState } from "react";
+
 
 function App(){
-    return (
-    <BrowserRouter>
-        <nav>
-            <Link to="/">home</Link>
-            <Link to="/movie">movie</Link>
-        </nav>
-        <Routes>
-            <Route path="/" element={<Example/>}></Route>
-            <Route path="/movie" element={<Home/>}></Route>
-            <Route path ="/movie/:id" element={<Detail/>} ></Route>
-        </Routes>
-    </BrowserRouter>
-    );
+
+    /*
+    * use state and Effect
+    */
+    const [Usrid,setUsrid] = useState("");
+    const [UsrPw,setHours] = useState("");
+
+    useEffect(() =>{
+    
+    },[])
+
+    /*
+    * User Function
+    */
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`id : ${Usrid} pw : ${UsrPw}`)
+        if(Usrid === null || Usrid === "") {
+            alert("id를 입력 해 주세요");
+            return;
+        }
+        if(UsrPw === null || UsrPw === "") {
+            alert("비밀번호를 입력 해 주세요");
+            return;
+        }
+
+        if(Usrid === user.id && UsrPw === user.pw) alert("oo")
+        else alert("유저가 존재하지 않거나 비밀번호가 틀렸어요")
+        
+    }
+
+    var user = {
+        id : "1234",
+        pw : "qwer"
+    }
+
+    /*
+    * HTML {javascript}
+    */
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="Usrid">ID | </label>
+                <input
+                    value={Usrid || ""}
+                    placeholder="Usrid"
+                    id="Usrid"
+                    type="text"
+                    onChange={(e) => setUsrid(e.target.value)}
+                >
+                </input>
+                <label htmlFor="UsrPw">UsrPw</label>
+                <input
+                    placeholder="UsrPw"
+                    type="password"
+                    id="UsrPw"
+                    value={UsrPw || ""}
+                    onChange={(e) => setHours(e.currentTarget.value)}
+                >
+                </input>
+                <button type="submit">Login</button>
+            </form>
+
+        </div>
+    )
 }
 
 export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,24 +1,9 @@
 import { useState, useEffect } from "react";
-import Movie from "../components/Movie";
 
 function Home(){
     /*
     * use state and Effect
     */
-    const [loading,setloading] = useState(true);
-    const [movies, setMovies] = useState([]);
-
-
-    useEffect(() => {
-        fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
-        .then(response => response.json())
-        .then(json => {
-                setMovies(json.data.movies);
-                setloading(false);
-            })
-
-    },[])
-
 
     /*
     * User Function
@@ -29,19 +14,7 @@ function Home(){
     */
     return(
         <div>
-            {loading ? 
-                <h1>Loading</h1> : 
-                <div>{movies.map(movie => 
-                    <Movie
-                        key={movie.id}//중요
-                        id={movie.id}
-                        medium_cover_image={movie.medium_cover_image}
-                        title ={movie.title}
-                        summary = {movie.summary}
-                        genres = {movie.genres}
-                    />)}
-                </div>
-            }
+            
         </div>
     )
 }
